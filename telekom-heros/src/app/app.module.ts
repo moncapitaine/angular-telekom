3be2ecf4b81bd49aa123d083743631f88401cff2
Michael Vogt <michael.vogt@progwise.net>
2023-02-27 16:11:42 +0100

    routing

--- telekom-heros/src/app/app.module.ts
+++ telekom-heros/src/app/app.module.ts
@@ -6,9 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailsComponent } from './heroes/hero-details/hero-details.component';
+import { HeroListComponent } from './pages/hero-list/hero-list.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailsComponent],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailsComponent,
+    HeroListComponent,
+    HomeComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ telekom-heros/src/app/pages/hero-list/hero-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tk-toh-hero-list',
+  templateUrl: './hero-list.component.html',
+  styleUrls: ['./hero-list.component.scss']
+})
+export class HeroListComponent {
+
+}
