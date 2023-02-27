d3e093e10bf03a1d1ed0cb1cd6854a00b6d0eac6
Michael Vogt <michael.vogt@progwise.net>
2023-02-27 14:08:10 +0100

    cleanup

--- telekom-heros/src/app/heroes/hero-details/hero-details.component.ts
+++ telekom-heros/src/app/heroes/hero-details/hero-details.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Hero } from 'src/app/domain/hero';
 
 @Component({
-  selector: 'app-hero-details',
+  selector: 'tk-toh-hero-details',
   templateUrl: './hero-details.component.html',
   styleUrls: ['./hero-details.component.scss'],
 })
