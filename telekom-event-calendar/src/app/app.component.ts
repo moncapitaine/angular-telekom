09218eec1776e261ef6481702629cc7749b11cbd
Michael Vogt <michael.vogt@progwise.net>
2023-03-20 11:43:53 +0100

    telekom event calendar

---
+++ telekom-event-calendar/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // template: '<h2>Geht auch...</h2>',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'telekom-event-calendar';
+}
