a51c55e0ec6e7413156eec62f68b653e3e799e21
Michael Vogt <michael.vogt@progwise.net>
2023-03-20 11:59:29 +0100

    ngfor

--- telekom-event-calendar/src/app/components/event-list/event-list.component.ts
+++ telekom-event-calendar/src/app/components/event-list/event-list.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.component.html',
-  styleUrls: ['./event-list.component.css']
+  styleUrls: ['./event-list.component.css'],
 })
 export class EventListComponent {
-
+  eventData: string[] = ['event2', 'event2', 'event3'];
 }
