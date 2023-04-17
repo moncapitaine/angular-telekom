dcbcbbcb85907f6701a7896b2ac1abb669f5a1bb
Michael Vogt <michael.vogt@progwise.net>
2023-04-17 11:43:57 +0200

    personName

--- cookbook/src/app/app.module.ts
+++ cookbook/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MichaelComponent } from './components/michael/michael.component';
+import { PersonComponent } from './components/person/person.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MichaelComponent
+    MichaelComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule
---
+++ cookbook/src/app/components/person/person.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-person',
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.css']
+})
+export class PersonComponent {
+  @Input() personName: string | undefined;
+}
