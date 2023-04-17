84b68e20088959971d4fdd8364e593ab92f76227
Michael Vogt <michael.vogt@progwise.net>
2023-04-17 11:27:20 +0200

    app-michael

--- cookbook/src/app/app.module.ts
+++ cookbook/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MichaelComponent } from './components/michael/michael.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MichaelComponent
   ],
   imports: [
     BrowserModule
---
+++ cookbook/src/app/components/michael/michael.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-michael',
+  templateUrl: './michael.component.html',
+  styleUrls: ['./michael.component.css']
+})
+export class MichaelComponent {
+
+}
